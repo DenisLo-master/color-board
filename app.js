bee52e4ebf26eb0b0364e746966d2938b561888c
DENIS LOGINOV <denis.lkg@gmail.com>
2023-01-12 19:45:00 +0600

    refactoring

--- app.js
+++ app.js
@@ -5,20 +5,24 @@ const colors = ["#e34c3c", "#6ee33d", "#3e44ad", "#4caaad", '#3455db', "#e67f22"
 for (let i = 0; i < COUNT_BLOCK; i++) {
     const block = document.createElement("div")
     block.className = "block"
-    block.addEventListener("mouseenter", () => {
-        const color = getColor()
-        block.style.backgroundColor = color
-        block.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`
-    })
-    block.addEventListener("mouseleave", () => {
-        block.style.backgroundColor = "#1d1d1d"
-        block.style.boxShadow = `0 0 2px #000`
-    })
+    block.addEventListener("mouseenter", setColor)
+    block.addEventListener("mouseleave", removeColor)
     board.append(block)
 }
 
-function getColor() {
+function getRandomColor() {
     const index = Math.floor(Math.random() * (colors.length - 1))
     const color = colors[index]
     return color
+}
+
+function setColor() {
+    const color = getRandomColor()
+    block.style.backgroundColor = color
+    block.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`
+}
+
+function removeColor() {
+    block.style.backgroundColor = "#1d1d1d"
+    block.style.boxShadow = `0 0 2px #000`
 }
